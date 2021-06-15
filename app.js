0ac815a5e60bf2de502c23f37326073c50e20619
Munzir <munzirsiddiqui31@gmail.com>
2021-06-16 01:10:08 +0530

    adding database

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 
-mongoose.connect('mongodb://localhost:27017/todolistDB', { useNewUrlParser: true })
+mongoose.connect('mongodb+srv://admin-munzir:test123@cluster0.gip8h.mongodb.net/todolistDB', { useNewUrlParser: true })
 
 const itemsSchema = new mongoose.Schema({
     name: {
